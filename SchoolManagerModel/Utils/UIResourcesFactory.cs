6d5619cb6c5db578ad3f00561669834ed5f1ad33
TheKornelM <makari.kornel@gmail.com>
2024-11-17 16:23:46 +0100

    Add static resource manager factory class

---
+++ SchoolManagerModel/Utils/UIResourcesFactory.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Resources;
+
+namespace SchoolManagerModel.Utils;
+
+public static class UIResourceFactory
+{
+    public static ResourceManager GetNewResource()
+    {
+        return new ResourceManager("SchoolManagerModel.Resources.UI",
+                Assembly.GetExecutingAssembly());
+    }
+}
