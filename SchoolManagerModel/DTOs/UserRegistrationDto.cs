f98ec72065ad6f94ff87e32f7f0c3c60a0f92873
TheKornelM <makari.kornel@gmail.com>
2024-12-11 19:26:45 +0100

    User registration DTO

---
+++ SchoolManagerModel/DTOs/UserRegistrationDto.cs
@@ -0,0 +1,35 @@
+﻿using SchoolManagerModel.Utils;
+
+namespace SchoolManagerModel.DTOs;
+public class UserRegistrationDto
+{
+    public string Username { get; set; }
+    public string Email { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Name => CultureUtils.GetFullName(FirstName, LastName);
+
+    public string Role { get; set; }
+
+    public string SelectedClassId { get; set; }
+
+    public string Password { get; set; }
+
+    public UserRegistrationDto()
+    {
+
+    }
+
+    public UserRegistrationDto(string username, string email, string firstName, string lastName, string role, string password)
+    {
+        Username = username;
+        Email = email;
+        FirstName = firstName;
+        LastName = lastName;
+        Role = role;
+        Password = password;
+    }
+}
