db62a23a4315f18bbbdde0b43038ec7c57ba1cf0
TheKornelM <makari.kornel@gmail.com>
2024-12-17 21:38:19 +0100

    Use DbContextFactory for SubjectDatabase

--- SchoolManagerModel/Persistence/SubjectDatabase.cs
+++ SchoolManagerModel/Persistence/SubjectDatabase.cs
@@ -4,10 +4,11 @@ using SchoolManagerModel.Entities.UserModel;
 
 namespace SchoolManagerModel.Persistence;
 
-public class SubjectDatabase(SchoolDbContextBase dbContext) : IAsyncSubjectDataHandler
+public class SubjectDatabase(IDbContextFactory<SchoolDbContext> dbContextFactory) : IAsyncSubjectDataHandler
 {
     public async Task<List<Mark>> GetStudentSubjectMarksAsync(Student student, Subject subject)
     {
+        await using var dbContext = await dbContextFactory.CreateDbContextAsync();
         return await dbContext.Marks
             .Where(x => x.Subject == subject && x.Student == student)
             .ToListAsync();
@@ -15,6 +16,7 @@ public class SubjectDatabase(SchoolDbContextBase dbContext) : IAsyncSubjectDataH
 
     public async Task<bool> IsAssignedSubjectToStudentAsync(User student, Subject subject)
     {
+        await using var dbContext = await dbContextFactory.CreateDbContextAsync();
         return await dbContext.AssignedSubjects
             .Include(x => x.Student.User)
             .AnyAsync(x => x.Student.User.Id == student.Id && x.Subject.Id == subject.Id);
@@ -22,6 +24,7 @@ public class SubjectDatabase(SchoolDbContextBase dbContext) : IAsyncSubjectDataH
 
     public async Task AddMarkAsync(Mark mark)
     {
+        await using var dbContext = await dbContextFactory.CreateDbContextAsync();
         dbContext.Attach(mark);
         await dbContext.Marks.AddAsync(mark);
         await dbContext.SaveChangesAsync();
@@ -29,13 +32,18 @@ public class SubjectDatabase(SchoolDbContextBase dbContext) : IAsyncSubjectDataH
 
     public async Task AddSubjectAsync(Subject subject)
     {
-        dbContext.Attach(subject);
+        await using var dbContext = await dbContextFactory.CreateDbContextAsync();
+        dbContext.Attach(subject.Teacher);
+        dbContext.Attach(subject.Class);
+
         await dbContext.Subjects.AddAsync(subject);
         await dbContext.SaveChangesAsync();
     }
 
     public async Task AssignSubjectsToStudentAsync(Student student, List<Subject> subjects)
     {
+        await using var dbContext = await dbContextFactory.CreateDbContextAsync();
+
         var dbStudent = dbContext.Students
             .Where(x => x.Id == student.Id)
             .Select(x => new
@@ -54,7 +62,6 @@ public class SubjectDatabase(SchoolDbContextBase dbContext) : IAsyncSubjectDataH
         var dbSubjects = await dbContext.Subjects
             .Where(s => subjects.Select(sub => sub.Id).Contains(s.Id))
             .ToListAsync();
-
         foreach (var dbSubject in dbSubjects)
         {
             if (dbStudent.Subjects.Any(subject => subject.Subject.Id == dbSubject.Id))
@@ -67,31 +74,29 @@ public class SubjectDatabase(SchoolDbContextBase dbContext) : IAsyncSubjectDataH
                 Subject = dbSubject,
                 Student = dbStudent.Student
             });
-
         }
 
         await dbContext.SaveChangesAsync();
     }
 
-
     public async Task<List<Student>> GetSubjectStudentsAsync(Subject subject)
     {
-        var students = await dbContext.AssignedSubjects
+        await using var dbContext = await dbContextFactory.CreateDbContextAsync();
+        return await dbContext.AssignedSubjects
             .Include(x => x.Student)
             .ThenInclude(x => x.User)
             .Where(x => x.Subject == subject)
             .Select(x => x.Student)
             .ToListAsync();
-
-        return students;
     }
 
     public async Task<List<Mark>> GetStudentMarksAsync(Student student)
     {
+        await using var dbContext = await dbContextFactory.CreateDbContextAsync();
         return await dbContext.Marks
             .Include(x => x.Subject)
-                .ThenInclude(x => x.Teacher)
-                    .ThenInclude(x => x.User)
+            .ThenInclude(x => x.Teacher)
+            .ThenInclude(x => x.User)
             .Where(x => x.Student == student)
             .ToListAsync();
     }
